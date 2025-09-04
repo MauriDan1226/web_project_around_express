00a9ebb5ebab102b162e2071886410aed426fdd0
titanram <mdrg.mauricio@outlook.com>
2025-09-04 12:37:40 -0600

    fin

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Tripleten web_project_around_express
 
-ejmlo
+📌 Nombre del proyecto: Página Web Around the US - Tripleten
+📖 Descripción del proyecto
+
+Este proyecto consiste en la creación de un servidor con Express.js, encargado de proporcionar datos de usuarios y tarjetas mediante endpoints.
+
+**\*Funcionalidades principales:\*\***
+
+\*Configuración de un servidor con Express.js.
+
+\*Acceso al servidor desde el navegador o herramientas como Postman.
+
+\***_Creación de rutas para obtener información de_**:
+
+*Usuarios
+*Tarjetas
+
+\***_🛠️ Tecnologías utilizadas _**
+
+\*Node.js → Lenguaje principal para la lógica del backend.
+
+\*Express.js → Framework para la creación del servidor y manejo de rutas.
+
+**\* Diseñada por Mauricio Ramirez \*\***
--- app.js
+++ app.js
@@ -2,11 +2,55 @@ const express = require("express");
 const app = express();
 const { PORT = 3000 } = process.env;
 
+const fs = require("fs");
+const path = require("path");
+
+// RUTA: Lista de cartas --------
+
+app.use(express.json());
+app.get("/cards", (req, res) => {
+  const filePath = path.join(__dirname, "./data/cards.json");
+  const data = fs.readFileSync(filePath, "utf8");
+  const cards = JSON.parse(data);
+  res.json(cards);
+});
+
+// RUTA: Lista de usuarios --------
+
+app.get("/users", (req, res) => {
+  const filePath = path.join(__dirname, "./data/users.json");
+  const data = fs.readFileSync(filePath, "utf8");
+  const users = JSON.parse(data);
+  res.json(users);
+});
+
+//  RUTA: Usuario por ID --------
+
+app.get("/users/:id", (req, res) => {
+  const filePath = path.join(__dirname, "./data/users.json");
+  const data = fs.readFileSync(filePath, "utf8");
+  const users = JSON.parse(data);
+
+  const user = users.find((u) => u.id === req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "ID de usuario no encontrado" });
+  }
+
+  res.json(user);
+});
+
+//  MANEJO DE RUTAS NO EXISTENTES --------
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Recurso solicitado no encontrado" });
+});
+
 app.get("/", (req, res) => {
   res.send("Servidor Express funcionando en el puerto 3000");
 });
 
 // Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}...`);
+  console.log(`App listening at port ${PORT}`);
 });
